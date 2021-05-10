a419e3d722ea1efee7c4b46310c1fb44356e2678
HEYHO8037 <haeho8037@gmail.com>
2021-05-10 22:02:45 +0900

    Lab2_test6

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,72 +1,71 @@
 #include "Lab2.h"
 
+using namespace std;
+
 namespace lab2
 {
-    void PrintIntegers(std::istream& in, std::ostream& out)
-    {
-        int number;
-        int count = 0;
-        std::string nonNumberTrash;
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		int number;
 
-        while (!in.eof())
-        {
-            in >> number;
+		string nonNumberTrash;
 
-            if (in.fail())
-            {
-                in.clear();
-                in >> nonNumberTrash;
-            }
-            else
-            {
-                if (count == 0)
-                {
-                    out << "         out" << "        dec" << "      hex" << std::endl;
-                    out << "------------ " << "---------- " << "--------" << std::endl;
-                    count++;
-                }
+		out << "         out" << "        dec" << "      hex" << endl;
+		out << "------------ " << "---------- " << "--------" << endl;
 
-                out << std::setfill(' ') << std::setw(12) << std::oct << std::right << number;
-                out << std::setfill(' ') << std::setw(11) << std::dec << std::right << number;
-                out << std::setfill(' ') << std::setw(9) << std::hex << std::right << number << std::endl;
-                continue;
-            }
-        }
-    }
+		while (!in.eof())
+		{
+			in >> number;
 
-    void PrintMaxFloat(std::istream& in, std::ostream& out)
-    {
-        float saveMaxNumber = 0.0f;
-        float number;
+			if (in.fail())
+			{
+				in.clear();
+				in >> nonNumberTrash;
+			}
+			else
+			{
+				out << std::setfill(' ') << setw(12) << oct << right << number;
+				out << std::setfill(' ') << setw(11) << dec << right << number;
+				out << std::setfill(' ') << setw(9) << uppercase << hex << right << number << endl;
+				continue;
+			}
+		}
+	}
 
-        std::string nonNumberTrash;
 
-        while (!in.eof())
-        {
-            in >> number;
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		float saveMaxNumber = 0.0f;
+		float number;
 
-            if (in.get() != '.' && in.fail())
-            {
-                in.clear();
-                in >> nonNumberTrash;
-            }
-            else
-            {
-                if (saveMaxNumber == 0 || saveMaxNumber < number)
-                {
-                    saveMaxNumber = number;
-                }
+		string nonNumberTrash;
 
-                out << std::fixed;
-                out.precision(3);
-                out << std::setfill(' ') << std::setw(5) <<  "" << std::setw(15) << std::internal << std::showpos << number << std::endl;
-                continue;
-            }
-        }
+		while (!in.eof())
+		{
+			in >> number;
 
-        out << std::fixed;
-        out.precision(3);
-        out << "max: " << std::setw(15) << std::internal << std::showpos << saveMaxNumber << std::endl;
+			if (in.fail())
+			{
+				in.clear();
+				in >> nonNumberTrash;
+			}
+			else
+			{
+				if (saveMaxNumber == 0 || saveMaxNumber < number)
+				{
+					saveMaxNumber = number;
+				}
 
-    }
+				out << fixed;
+				out.precision(3);
+				out << setfill(' ') << setw(5) << "" << setw(15) << internal << showpos << number << endl;
+				continue;
+			}
+		}
+
+		out << fixed;
+		out.precision(3);
+		out << "max: " << setw(15) << internal << showpos << saveMaxNumber << endl;
+
+	}
 }
