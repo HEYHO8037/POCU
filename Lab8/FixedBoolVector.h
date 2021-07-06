708b270f91750388260a03844329401d25ed353e
HEYHO8037 <haeho8037@gmail.com>
2021-07-06 17:09:54 +0900

    Lab8_9

--- Lab8/FixedBoolVector.h
+++ Lab8/FixedBoolVector.h
@@ -20,7 +20,8 @@ namespace lab8
 
 	private:
 		size_t mSize;
-		unsigned int mArray = { 0 };
+		unsigned int mArraySize = 0;
+		uint32_t mArray[N / 32 + 1] = { 0 };
 	};
 
 	template<size_t N>
@@ -54,16 +55,23 @@ namespace lab8
 		{
 			if (bAdd == true)
 			{
-				mArray |= 1UL << mSize;
+				mArray[mArraySize] |= 1UL << mSize % 32;
 				mSize++;
 			}
 			else
 			{
-				mArray &= ~(1UL << mSize);
+				mArray[mArraySize] &= ~(1UL << mSize % 32);
 				mSize++;
 			}
 
+			if (mSize % 32 == 0 && mSize != 0)
+			{
+				mArraySize++;
+			}
+
 		}
+
+		return true;
 	}
 
 
@@ -71,17 +79,26 @@ namespace lab8
 	bool FixedVector<bool, N>::Remove(const bool& bRemove)
 	{
 		unsigned int saveLength;
+		unsigned int arrayLength = 0;
 		bool bFind = false;
 
 		for (unsigned int length = 0; length < mSize; length++)
 		{
-			if ((mArray >> length & 1U) == bRemove)
+			if ((mArray[arrayLength] >> (length % 32) & 1U) == bRemove)
 			{
 				saveLength = length;
 				bFind = true;
 				break;
 			}
+
+			if (length % 32 == 0 && length != 0)
+			{
+				arrayLength++;
+
+			}
 		}
+
+		arrayLength = 0;
 
 		if (bFind == false)
 		{
@@ -89,18 +106,38 @@ namespace lab8
 		}
 		else
 		{
-			for (unsigned int length = saveLength; length < mSize - 1; length++)
+			for (unsigned int length = saveLength; length < mSize; length++)
 			{
-				if ((mArray >> (length + 1) & 1U) == true)
+				if (length % 32 == 0 && length != 0)
 				{
-					mArray |= 1UL << length;
+					arrayLength++;
 				}
-				else
+
+				if (length % 32 < 31)
 				{
-					mArray &= ~(1UL << length);
+					if ((mArray[arrayLength] >> (length % 32 + 1) & 1U) == true)
+					{
+						mArray[arrayLength] |= 1UL << (length % 32);
+					}
+					else
+					{
+						mArray[arrayLength] &= ~(1UL << (length % 32));
+					}
 				}
-				
+				else if (length % 32 == 31)
+				{
+					if ((mArray[arrayLength + 1] >> (length % 32 + 1) & 1U) == true)
+					{
+						mArray[arrayLength] |= 1UL << (length % 32);
+					}
+					else
+					{
+						mArray[arrayLength] &= ~(1UL << (length % 32));
+					}
+
+				}
 			}
+
 
 			mSize--;
 			return true;
@@ -110,23 +147,31 @@ namespace lab8
 	template<size_t N>
 	bool FixedVector<bool, N>::Get(unsigned int index)
 	{
-		return (mArray >> index) & 1U;
+		unsigned int size = index / 32;
+		return (mArray[size] >> index % 32) & 1U;
 	}
 
 	template<size_t N>
 	bool FixedVector<bool, N>::operator[](unsigned int index)
 	{
-		return (mArray >> index) & 1U;
+		unsigned int size = index / 32;
+		return (mArray[size] >> index % 32) & 1U;
 	}
 
 	template<size_t N>
 	int FixedVector<bool, N>::GetIndex(const bool& bIndex)
 	{
 		int index = -1;
+		unsigned int arrayLength = 0;
 
 		for (unsigned int length = 0; length < mSize; length++)
 		{
-			if (((mArray >> length) & 1U) == bIndex)
+			if (length % 32 == 0 && length != 0)
+			{
+				arrayLength++;
+			}
+
+			if (((mArray[arrayLength] >> length % 32) & 1U) == bIndex)
 			{
 				index = length;
 				break;
